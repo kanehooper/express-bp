882868d038e562dbfef74c6b8f73e4b873e45419
Kane Hooper <kanehooper@Kanes-MacBook-Pro.local>
2019-08-10 22:03:52 +1000

    Add home page

--- config/middleware.js
+++ config/middleware.js
@@ -9,12 +9,12 @@ const app = require('./server')
  * Module dependencies
  */
 const helmet = require('helmet')
+const logger = require('morgan')
 const expressStatusMonitor = require('express-status-monitor')
 const path = require('path')
 const favicon = require('serve-favicon')
 const compression = require('compression')
 const express = require('express')
-const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 
 /**
@@ -22,13 +22,13 @@ const cookieParser = require('cookie-parser')
  */
 const addMiddleware = () => {
     app.use(helmet())
+    app.use(logger('dev'))
     app.use(expressStatusMonitor()) // Real time metrics available at /status
     app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')))
     app.use(compression())
     app.use(express.static('public'))
     app.use(express.urlencoded({extended: true}))
     app.use(express.json())
-    app.use(logger('dev'))
     app.use(cookieParser())
     app.disable('x-powered-by')
 }
